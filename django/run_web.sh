d3b16525c489d878be403a93305fcbef90b48d59
Pablo Chico de Guzman <pchico83@gmail.com>
2019-06-26 12:43:37 +0200

    Fix small issues in django sample

--- django/run_web.sh
+++ django/run_web.sh
@@ -7,14 +7,14 @@ do
     sleep 1
 done
 
-echo "Database is ready..."
+echo "Database is ready"
 cd myproject
 
 # prepare init migration
 python manage.py makemigrations myproject
-echo "Created migrations..."
+echo "Created migrations"
 # migrate db, so we have the latest db schema
 python manage.py migrate
-echo "Migrated DB to latest version..."
+echo "Migrated DB to latest version"
 # start development server on public ip interface, on port 8080
 python manage.py runserver 0.0.0.0:8080
