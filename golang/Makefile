c5cd1585a75128d48357f77b92be862a1a9f871b
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-08-27 18:37:25 -0700

    add debugger to the sample

---
+++ golang/Makefile
@@ -0,0 +1,5 @@
+start:
+	go run main.go
+
+debug:
+	dlv debug --headless --listen=:2345 --log --api-version=2
--- golang/main.go
+++ golang/main.go
@@ -8,26 +8,22 @@ import (
 
 const namespaceFile = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
 
+var namespace = ""
+
 func main() {
 	fmt.Println("Starting hello-world server...")
+	bytes, err := ioutil.ReadFile(namespaceFile)
+	if err != nil {
+		panic(fmt.Sprintf("Looks like you are not running inside a Kubernetes Pod: %s", err))
+	}
+
+	namespace = string(bytes)
 	http.HandleFunc("/", helloServer)
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received request")
-	namespace, err := getNamespace()
-	if err != nil {
-		fmt.Fprint(w, err)
-	} else {
-		fmt.Fprint(w, fmt.Sprintf("Hello from the cluster namespace '%s'", namespace))
-	}
-}
-
-func getNamespace() (string, error) {
-	bytes, err := ioutil.ReadFile(namespaceFile)
-	if err != nil {
-		return "", fmt.Errorf("Looks like you are not running inside a Kubernetes Pod")
-	}
-	return string(bytes), nil
+	fmt.Fprint(w, fmt.Sprintf("Hello from the Cloud Native Meetup '%s'", namespace))
 }
